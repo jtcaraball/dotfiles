5515157d16541ac673cf1618b5e6c9d0da72109c
jo <jtcaraballm@gmail.com>
2025-06-04 19:53:35 -0400

    feat: display only the lsp clients attached to current buf.

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,3 +1,47 @@
+--- @nodoc
+--- @param buf_id integer
+--- @param client vim.lsp.Client
+--- @return boolean
+local function buf_in_client(buf_id, client)
+	for id, _ in pairs(client.attached_buffers) do
+		if buf_id == id then
+			return true
+		end
+	end
+	return false
+end
+
+--- @nodoc
+--- @return string
+local function clients_lsp()
+	local clients = vim.lsp.get_clients()
+	local curr_buf = vim.api.nvim_get_current_buf()
+	if next(clients) == nil then
+		return ""
+	end
+
+	local c = {}
+	for _, client in pairs(clients) do
+		if buf_in_client(curr_buf, client) then
+			table.insert(c, client.name)
+		end
+	end
+
+	local out = table.concat(c, "|")
+	return (vim.o.columns > 100 and "   LSP ~ " .. out .. " ") or "   LSP "
+end
+
+local function mode_theme(colors, mode_color)
+	return {
+		a = { bg = mode_color, fg = colors.mantle, gui = "bold" },
+		b = { bg = colors.surface1, fg = colors.subtext0 },
+		c = { bg = colors.none, fg = colors.overlay1 },
+		x = { bg = colors.none },
+		y = { bg = colors.none, fg = colors.subtext0 },
+		z = { bg = colors.surface1, fg = colors.subtext0 },
+	}
+end
+
 return {
 	'nvim-lualine/lualine.nvim',
 	dependencies = {
@@ -6,38 +50,14 @@ return {
 	config = function ()
 		local colors = require("catppuccin.palettes").get_palette()
 		colors.none = "NONE"
-		local function clients_lsp()
-			local clients = vim.lsp.get_clients()
-			if next(clients) == nil then
-				return ""
-			end
-
-			local c = {}
-			for _, client in pairs(clients) do
-				table.insert(c, client.name)
-			end
-
-			local out = table.concat(c, "|")
-			return (vim.o.columns > 100 and "   LSP ~ " .. out .. " ") or "   LSP "
-		end
-		local function mode_theme(mode_color)
-			return {
-				a = { bg = mode_color, fg = colors.mantle, gui = "bold" },
-				b = { bg = colors.surface1, fg = colors.subtext0 },
-				c = { bg = colors.none, fg = colors.overlay1 },
-				x = { bg = colors.none },
-				y = { bg = colors.none, fg = colors.subtext0 },
-				z = { bg = colors.surface1, fg = colors.subtext0 },
-			}
-		end
 		local theme = {
-			normal = mode_theme(colors.blue),
-			insert = mode_theme(colors.green),
-			terminal = mode_theme(colors.green),
-			command = mode_theme(colors.peach),
-			visual = mode_theme(colors.mauve),
-			replace = mode_theme(colors.red),
-			inactive = mode_theme(colors.blue),
+			normal = mode_theme(colors, colors.blue),
+			insert = mode_theme(colors, colors.green),
+			terminal = mode_theme(colors, colors.green),
+			command = mode_theme(colors, colors.peach),
+			visual = mode_theme(colors, colors.mauve),
+			replace = mode_theme(colors, colors.red),
+			inactive = mode_theme(colors, colors.blue),
 		}
 		local options = {
 			options = {
