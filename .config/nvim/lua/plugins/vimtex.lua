eea006e64f4f333ed6649323cd5fbfa146c9ba0d
jo <jtcaraballm@gmail.com>
2025-07-19 02:34:39 -0400

    feat: bye bye vimtex.

--- .config/nvim/lua/plugins/vimtex.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"lervag/vimtex",
-	lazy = false,
-	init = function ()
-		vim.g.vimtex_syntax_enabled = 1
-		vim.g.vimtex_quickfix_open_on_warning = 0
-		vim.g.vimtex_syntax_conceal_disable = 1
-	end,
-}
