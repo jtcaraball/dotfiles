025e7c3ad4058143ac1e0d44bde169a6fc80c6d8
jo <jtcaraballm@gmail.com>
2025-09-29 21:46:13 -0300

    feat: add pep8 formater and make some styling refactors.

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -4,30 +4,35 @@ return {
 	build = 'cargo build --release',
 	opts = {
 		-- See :h blink-cmp-config-keymap for defining your own keymap
-	keymap = {
-		preset = 'default',
-		['<Tab>'] = { 'select_and_accept', 'fallback' }
-	},
+		keymap = {
+			preset = 'default',
+			['<Tab>'] = { 'select_and_accept', 'fallback' }
+		},
 		appearance = {
 			-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
 			-- Adjusts spacing to ensure icons are aligned
 			nerd_font_variant = 'mono'
 		},
 		completion = {
-		menu = { border = "rounded" },
-		documentation = {
-			auto_show = true,
-			window = { border = "rounded" },
+			menu = { border = "rounded" },
+			documentation = {
+				auto_show = true,
+				window = { border = "rounded" },
+			},
 		},
-	},
 		-- Default list of enabled providers defined so that you can extend it
 		-- elsewhere in your config, without redefining it, due to `opts_extend`
 		sources = {
 			default = { 'lsp', 'path', 'snippets', 'buffer' },
 			providers = {
 				lsp = { score_offset = 9001 },
-				snippets = { score_offset = -9001 },
-			}
+				snippets = {
+					score_offset = -9001,
+					should_show_items = function(ctx)
+						return ctx.trigger.initial_kind ~= 'trigger_character'
+					end
+				},
+			},
 		},
 		fuzzy = { implementation = "prefer_rust_with_warning" }
 	},
--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -6,6 +6,7 @@ return {
 			markdown = { "prettier" },
 			yaml = { "prettier" },
 			vue = { "prettier" },
+			python = { "autopep8" },
 		},
 	},
 }
