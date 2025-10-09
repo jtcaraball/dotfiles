7df72add2f6c1f2f3c5a4d7d29f2f7283e2ced8b
jo <jtcaraballm@gmail.com>
2025-10-09 09:28:41 -0300

    feat: add cleanup and 'copy only if not empty' to nvim-input.

--- .config/hypr/nvim-input.sh
+++ .config/hypr/nvim-input.sh
@@ -13,5 +13,11 @@ TMPFILE="${TMPFILE}.${EXTENSION}"
 # Input
 $TERM nvim +startinsert +'autocmd BufWritePost <buffer> quit' "${TMPFILE}" || true
 
-# Move to clipboard
-cat "${TMPFILE}" | wl-copy -n
+# Move to clipboard if not empty
+CONTENT=$(cat "${TMPFILE}")
+if [[ ! -z "${CONTENT}" ]]; then
+	wl-copy -n "${CONTENT}"
+fi
+
+# Clean up
+rm "${TMPFILE}"
