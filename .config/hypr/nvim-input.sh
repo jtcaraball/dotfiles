1351ded62a3028282379081fbb6384c35ae03310
jo <jtcaraballm@gmail.com>
2025-10-09 09:06:58 -0300

    feat: the automatic copy/paste input dream is dead.

--- .config/hypr/nvim-input.sh
+++ .config/hypr/nvim-input.sh
@@ -5,38 +5,13 @@ set -euo  pipefail
 TERM="kitty"
 TMPFILE="/tmp/nvim-input-tmp"
 
-copy_content() {
-	hyprctl dispatch sendshortcut "CTRL,A,"
-	hyprctl dispatch sendshortcut "CTRL,C,"
-	hyprctl dispatch sendshortcut "CTRL SHIFT,HOME,"
-}
-
-paste_content() {
-	hyprctl dispatch sendshortcut "CTRL,A,"
-	hyprctl dispatch sendshortcut ",DELETE,"
-	hyprctl dispatch sendshortcut "CTRL,V,"
-}
-
-# Copy
-CLIPBOARD=$(wl-paste -n)
-sleep 0.5  # Wait for dispatched commands to their thing
-copy_content
-INPUT=$(wl-paste -n)
-
 # Ask for extension
 ROFI_STYLE="#prompt{enabled: true;} #listview{enabled: false;}"
 EXTENSION=$(echo "" | rofi -dmenu -p "Extension" -theme-str "${ROFI_STYLE}")
 TMPFILE="${TMPFILE}.${EXTENSION}"
 
-# Edit
-wl-paste --type TEXT >"${TMPFILE}"
+# Input
 $TERM nvim +startinsert +'autocmd BufWritePost <buffer> quit' "${TMPFILE}" || true
 
-# Paste
+# Move to clipboard
 cat "${TMPFILE}" | wl-copy -n
-paste_content
-
-# Restore
-sleep 0.5  # Wait for dispatched commands to their thing
-echo "${CLIPBOARD}" | wl-copy -n
-rm "${TMPFILE}"
