7dc46b8e9dc93530be8d38ebd3bf7d4076184d03
jo <jtcaraballm@gmail.com>
2026-04-09 13:39:42 -0400

    feat: consistent ' usage.

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,16 +1,16 @@
 return {
-	"stevearc/conform.nvim",
+	'stevearc/conform.nvim',
 	opts = {
 		formatters_by_ft = {
-			go = { "gofumpt" },
-			markdown = { "prettier" },
-			yaml = { "prettier" },
-			vue = { "prettier" },
-			typescript = { "prettier" },
-			typescriptreact = { "prettier" },
-			python = { "autopep8" },
-			sql = { "pg_format" },
-			typst = { "typstyle" }
+			go = { 'gofumpt' },
+			markdown = { 'prettier' },
+			yaml = { 'prettier' },
+			vue = { 'prettier' },
+			typescript = { 'prettier' },
+			typescriptreact = { 'prettier' },
+			python = { 'autopep8' },
+			sql = { 'pg_format' },
+			typst = { 'typstyle' }
 		},
 	},
 }
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,26 +1,27 @@
 local options = {
 	ensure_installed = {
-		"lua",
-		"luadoc",
-		"printf",
-		"vim",
-		"vimdoc",
-		"c",
-		"go",
-		"gitcommit",
-		"comment",
-		"javascript",
-		"tsx",
-		"vue",
-		"make",
-		"python",
-		"rust",
-		"latex",
+		'lua',
+		'luadoc',
+		'printf',
+		'vim',
+		'vimdoc',
+		'c',
+		'go',
+		'gitcommit',
+		'comment',
+		'javascript',
+		'tsx',
+		'vue',
+		'make',
+		'python',
+		'rust',
+		'latex',
+		'typst',
 	}
 }
 
 local function config (opts)
-	local ts = require("nvim-treesitter")
+	local ts = require('nvim-treesitter')
 	if opts.ensure_installed ~= nil then
 		ts.install(opts.ensure_installed)
 	end
@@ -31,9 +32,9 @@ local function config (opts)
 end
 
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	branch = "main",
+	'nvim-treesitter/nvim-treesitter',
+	build = ':TSUpdate',
+	branch = 'main',
 	lazy = false,
 	config = function ()
 		return config(options)
