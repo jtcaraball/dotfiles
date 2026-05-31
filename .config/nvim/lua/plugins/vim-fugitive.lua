75fdfa3e77cf682cb0149c1fe4c7fa603e65c676
jo <jtcaraballm@gmail.com>
2026-05-30 22:16:03 -0400

    feat(nvim): make vim-fugitive load at start.

--- .config/nvim/lua/plugins/vim-fugitive.lua
+++ .config/nvim/lua/plugins/vim-fugitive.lua
@@ -1,5 +1,6 @@
 return {
 	'tpope/vim-fugitive',
+	lazy = false,
 	keys = {
 		{ '<leader>Gs', '<cmd>G<CR><C-w>k<cmd>q<CR>', mode = 'n', desc = 'full git status' },
 		{ '<leader>Gd', '<cmd>Gvdiffsplit<CR>',       mode = 'n', desc = 'full git status' }
