64ab5f2f1eb54c7c900a8387aeefb766f6691fea
jo <jtcaraballm@gmail.com>
2026-08-22 00:49:09 -0400

    feat(hypr): move to lua conf.

---
+++ .config/hypr/hyprland.lua
@@ -0,0 +1,89 @@
+-- MONITOR(S) --
+
+hl.monitor({
+	output = 'HDMI-A-1',
+	mode = '3840x2160@144.05Hz',
+	position = '0x0',
+	scale = 'auto',
+})
+
+-- THEME --
+
+require('theme/windows')
+require('theme/animations')
+
+-- PROGRAMS --
+
+local terminal = 'kitty'
+local menu = 'rofi -show combi'
+local bar = '~/.config/waybar/launch.sh'
+
+-- AUTOSTART --
+
+hl.on('hyprland.start', function()
+	hl.exec_cmd('hyprpaper')
+	hl.exec_cmd('hypridle')
+end)
+
+-- INPUT --
+
+hl.config({
+	input = {
+		kb_layout = 'us, us',
+		kb_variant = ', alt-intl',
+		kb_model = '',
+		kb_options = 'grp:alt_shift_toggle',
+		kb_rules = '',
+		follow_mouse = 1,
+		sensitivity = 0, -- -1.0 - 1.0, 0 means no modification.
+	},
+})
+
+-- KEYBINDS --
+
+local mainMod = 'SUPER'
+
+-- Custom keybinds.
+hl.bind(mainMod .. ' + I', hl.dsp.exec_cmd(bar))
+hl.bind(mainMod .. ' + Space', hl.dsp.exec_cmd(menu))
+hl.bind(mainMod .. ' + Return', hl.dsp.exec_cmd(terminal))
+hl.bind(mainMod .. ' + A', hl.dsp.dpms())
+-- Navigation keybinds.
+hl.bind(mainMod .. ' + left', hl.dsp.focus({ direction = 'left' }))
+hl.bind(mainMod .. ' + right', hl.dsp.focus({ direction = 'right' }))
+hl.bind(mainMod .. ' + up', hl.dsp.focus({ direction = 'up' }))
+hl.bind(mainMod .. ' + down', hl.dsp.focus({ direction = 'down' }))
+-- We have forloops now.
+for i = 1, 10 do
+	local key = i % 10 -- 10 maps to key 0
+	hl.bind(mainMod .. ' + ' .. key, hl.dsp.focus({ workspace = i }))
+	hl.bind(mainMod .. ' + SHIFT + ' .. key, hl.dsp.window.move({ workspace = i }))
+end
+
+-- WINDOWRULES --
+
+hl.window_rule({
+	-- Ignore maximize requests from all apps. You'll probably like this.
+	name           = 'suppress-maximize-events',
+	match          = { class = '.*' },
+	suppress_event = 'maximize',
+})
+hl.window_rule({
+	-- Fix some dragging issues with XWayland
+	name     = 'fix-xwayland-drags',
+	match    = {
+		class      = '^$',
+		title      = '^$',
+		xwayland   = true,
+		float      = true,
+		fullscreen = false,
+		pin        = false,
+	},
+	no_focus = true,
+})
+hl.window_rule({
+	name  = 'move-hyprland-run',
+	match = { class = 'hyprland-run' },
+	move  = '20 monitor_h-120',
+	float = true,
+})
