8edc41fac602498ba76e37ad38928b5203e02397
jo <jtcaraballm@gmail.com>
2026-09-08 00:25:29 -0400

    feat(hypr): add window movement and volume keybinds.

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -1,9 +1,15 @@
 -- MONITOR(S) --
 
 hl.monitor({
-	output = 'HDMI-A-1',
-	mode = '3840x2160@144.05Hz',
+	output = 'DP-4',
+	mode = '2560x1440@143.97Hz',
 	position = '0x0',
+	scale = 'auto',
+})
+hl.monitor({
+	output = 'HDMI-A-2',
+	mode = '1920x1080@59.94Hz',
+	position = '2560x0',
 	scale = 'auto',
 })
 
@@ -53,12 +59,33 @@ hl.bind(mainMod .. ' + left', hl.dsp.focus({ direction = 'left' }))
 hl.bind(mainMod .. ' + right', hl.dsp.focus({ direction = 'right' }))
 hl.bind(mainMod .. ' + up', hl.dsp.focus({ direction = 'up' }))
 hl.bind(mainMod .. ' + down', hl.dsp.focus({ direction = 'down' }))
+-- Window management keybinds.
+hl.bind(mainMod .. ' + SHIFT + left', hl.dsp.window.move({ direction = 'left' }))
+hl.bind(mainMod .. ' + SHIFT + right', hl.dsp.window.move({ direction = 'right' }))
+hl.bind(mainMod .. ' + SHIFT + up', hl.dsp.window.move({ direction = 'up' }))
+hl.bind(mainMod .. ' + SHIFT + down', hl.dsp.window.move({ direction = 'down' }))
 -- We have forloops now.
 for i = 1, 10 do
 	local key = i % 10 -- 10 maps to key 0
 	hl.bind(mainMod .. ' + ' .. key, hl.dsp.focus({ workspace = i }))
 	hl.bind(mainMod .. ' + SHIFT + ' .. key, hl.dsp.window.move({ workspace = i }))
 end
+-- Volume keybinds.
+hl.bind(
+	'XF86AudioRaiseVolume',
+	hl.dsp.exec_cmd('wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+'),
+	{ repeating = true, locked = true }
+)
+hl.bind(
+	'XF86AudioLowerVolume',
+	hl.dsp.exec_cmd('wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-'),
+	{ repeating = true, locked = true }
+)
+hl.bind(
+	'XF86AudioMute',
+	hl.dsp.exec_cmd('wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle'),
+	{ locked = true }
+)
 
 -- WINDOWRULES --
 
